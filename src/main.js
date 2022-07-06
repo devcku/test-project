9a34f5a064adca98095262ee447a291c90cf18e0
Michael Chukwu <chukwumichael70@gmail.com>
2022-07-06 14:28:51 +0100

    base version

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import './index.css'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
