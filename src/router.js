d654595118bfcd734d35e6fb61f7c4087cb94f50
Michael Chukwu <chukwumichael70@gmail.com>
2022-08-05 11:31:03 +0100

    fixed build errors

--- src/router.js
+++ src/router.js
@@ -1,16 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./views/Home.vue";
-import Blog from "./views/Blog.vue";
-import Contact from "./views/Contact.vue";
 import Login from "./views/Login.vue";
 
 const routes = [
   { path: "/", redirect: "/home" },
   { path: "/home", component: Home },
   { path: "/login", component: Login },
-  { path: "/blog", component: Blog },
-  { path: "/contact", component: Contact },
 ];
 const router = createRouter({
   history: createWebHistory(),
